0fe3090f59adcdf3735e6b583d0d4b6c1c77ea7f
Stephen Booth <stephenboothdev@gmail.com>
2021-02-22 14:17:21 +0000

    Added random tests

--- binary-search/test/BinarySearch.test.js
+++ binary-search/test/BinarySearch.test.js
@@ -2,7 +2,7 @@ import * as assert from "assert";
 
 import BinarySearch, { InitializationError } from "../BinarySearch.js";
 
-describe("Binary Search", () => {
+describe("Binary Search TDD Tests", () => {
   it("instance of class can be created", () => {
     const binarySearch = new BinarySearch([1, 2, 3, 4]);
   });
@@ -89,4 +89,83 @@ describe("Binary Search", () => {
     const binarySearch10 = new BinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 400]);
     assert.strictEqual(binarySearch10.find(400), 9);
   });
+  it("returns -1 if item is not in array when using custom compare function", () => {
+    const binarySearch = new BinarySearch([1, 2, 3, 4]);
+    assert.strictEqual(binarySearch.find(5), -1);
+  });
+  it("finds item in array when using custom compare function", () => {
+    const binarySearch = new BinarySearch([1, 2, 3, 4]);
+    assert.notStrictEqual(binarySearch.find(1), -1);
+    const binarySearch2 = new BinarySearch([1, 2, 3, 4, 6, 7, 8, 9, 400]);
+    assert.notStrictEqual(binarySearch2.find(400), -1);
+  });
+
+  it("returns correct index of item found when using custom compare function", () => {
+    const binarySearch = new BinarySearch([4, 3, 2, 1], (a, b) => b - a);
+    assert.strictEqual(binarySearch.find(1), 3);
+    const binarySearch2 = new BinarySearch(
+      [400, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      (a, b) => b - a
+    );
+    assert.strictEqual(binarySearch2.find(2), 8);
+    const binarySearch3 = new BinarySearch(
+      [400, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      (a, b) => b - a
+    );
+    assert.strictEqual(binarySearch3.find(3), 7);
+    const binarySearch4 = new BinarySearch(
+      [400, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      (a, b) => b - a
+    );
+    assert.strictEqual(binarySearch4.find(4), 6);
+    const binarySearch5 = new BinarySearch(
+      [400, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      (a, b) => b - a
+    );
+    assert.strictEqual(binarySearch5.find(5), 5);
+    const binarySearch6 = new BinarySearch(
+      [400, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      (a, b) => b - a
+    );
+    assert.strictEqual(binarySearch6.find(6), 4);
+    const binarySearch7 = new BinarySearch(
+      [400, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      (a, b) => b - a
+    );
+    assert.strictEqual(binarySearch7.find(7), 3);
+    const binarySearch8 = new BinarySearch(
+      [400, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      (a, b) => b - a
+    );
+    assert.strictEqual(binarySearch8.find(8), 2);
+    const binarySearch9 = new BinarySearch(
+      [400, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      (a, b) => b - a
+    );
+    assert.strictEqual(binarySearch9.find(9), 1);
+    const binarySearch10 = new BinarySearch(
+      [400, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+      (a, b) => b - a
+    );
+    assert.strictEqual(binarySearch10.find(400), 0);
+  });
+});
+
+describe("Binary Search Randomized Tests", () => {
+  const numberOfTests = 100;
+  const maxArrayLength = 100;
+
+  for (let testIndex = 0; testIndex < numberOfTests; testIndex++) {
+    const randomArray = [];
+    for (let elementIndex = 0; elementIndex < maxArrayLength; elementIndex++) {
+      randomArray.push(Math.random());
+    }
+    const sortedArray = [...randomArray].sort();
+    const targetIndex = Math.ceil(Math.random() * sortedArray.length) - 1;
+    const targetElement = sortedArray[targetIndex];
+    it("finds target in random array", () => {
+      const binarySearch = new BinarySearch(sortedArray);
+      assert.strictEqual(binarySearch.find(targetElement), targetIndex);
+    });
+  }
 });
